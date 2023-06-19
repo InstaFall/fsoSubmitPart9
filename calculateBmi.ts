e252cf1f047efe1c0b9cf8daf7d82dc9fe463363
InstaFall <gorkemcanakilli@hotmail.com>
2023-06-20 00:08:22 +0300

    Commit exercise 9.5

--- calculateBmi.ts
+++ calculateBmi.ts
@@ -1,10 +1,10 @@
-const calculateBmi = (height: number, weight:number):string => {
-    const heightInMeters = height/100;
-    const bmi = weight / (heightInMeters * heightInMeters);
-    console.log(bmi)
-    let message = ''
-    
-    if(!isNaN(Number(height)) && !isNaN(Number(weight))) {
+export const calculateBmi = (height: number, weight: number): string => {
+  const heightInMeters = height / 100;
+  const bmi = weight / (heightInMeters * heightInMeters);
+  console.log(bmi)
+  let message = ''
+
+  if (!isNaN(Number(height)) && !isNaN(Number(weight))) {
     if (bmi < 0) throw new Error('Negative BMI Value!')
     if (bmi >= 0 && bmi < 18.5) message += 'Underweight (unhealthy)'
     if (bmi >= 18.5 && bmi < 25) message += 'Normal (healthy)'
@@ -12,40 +12,40 @@ const calculateBmi = (height: number, weight:number):string => {
     if (bmi >= 30 && bmi < 35) message += 'Obese Class I (unhealthy)'
     if (bmi >= 35 && bmi < 40) message += 'Obese Class II (very unhealthy)'
     if (bmi >= 40) message += 'Obese Class III (extremely unhealthy)'
-    } else {
-        throw new Error('Invalid input, Enter valid numbers!')
-    }
-    return message;
+  } else {
+    throw new Error('Invalid input, Enter valid numbers!')
+  }
+  return message;
 }
 
-    interface parseBmiResult {
-        height: number,
-        weight: number
-    }
-  const parseBmiArguments = (args: Array<string>): parseBmiResult => {
-    if (args.length < 4) throw new Error('Not enough arguments');
-    if (args.length > 4) throw new Error('Too many arguments');
-  
-    if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
-        return {
-            height: Number(args[2]),
-            weight: Number(args[3])
-        }   
-      } else {
-        throw new Error('Provided values were not numbers!');
-      }
-  };
+interface parseBmiResult {
+  height: number,
+  weight: number
+}
+const parseBmiArguments = (args: Array<string>): parseBmiResult => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if (args.length > 4) throw new Error('Too many arguments');
 
-  try {
-    const { height, weight } = parseBmiArguments(process.argv);
-    console.log(calculateBmi(height, weight));
-  } 
-  catch (e) {
-    let message = 'Something went wrong! '
-    if (e instanceof Error) message += e.message
-    console.log(e.message);
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      height: Number(args[2]),
+      weight: Number(args[3])
+    }
+  } else {
+    throw new Error('Provided values were not numbers!');
   }
-  
+};
+
+try {
+  const { height, weight } = parseBmiArguments(process.argv);
+  console.log(calculateBmi(height, weight));
+}
+catch (e) {
+  let message = 'Something went wrong! '
+  if (e instanceof Error) message += e.message
+  console.log(e.message);
+}
+
 
 /* // Hardcoded
 try {
--- index.ts
+++ index.ts
@@ -1,12 +1,35 @@
 import express from 'express';
+import { calculateBmi } from './calculateBmi';
+
 const app = express();
 
 app.get('/hello', (_req, res) => {
-  res.send('Hello Full Stack!');
+    res.send('Hello Full Stack!');
+});
+
+app.get('/bmi', (req, res) => {
+    const { height, weight } = req.query
+    if (!height || !weight) {
+        res.status(400).json({ error: 'malformatted parameters' });
+        return;
+    }
+    const n_Height = Number(height);
+    const n_Weight = Number(weight);
+
+    if (isNaN(n_Height) || isNaN(n_Weight)) {
+        res.status(400).json({ error: 'Malformatted parameters' });
+        return;
+    }
+
+    try {
+        const bmi = calculateBmi(n_Height, n_Weight);
+        res.json({ weight: n_Weight, height: n_Height, bmi });
+    } catch (e) {
+        res.status(500).json({ error: 'malformatted parameters' });
+    }
 });
 
 const PORT = 3003;
-
-app.listen(PORT,() => {
+app.listen(PORT, () => {
     console.log(`Server running at ${PORT}`);
 })
